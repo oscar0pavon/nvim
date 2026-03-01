f6463900a2785968309b81316a2bd6d0da943989
Oscar Pavon <pavon_oscar@outlook.com>
2026-03-01 00:40:15 -0300

    Init config file with color scheme

---
+++ init.lua
@@ -0,0 +1,23 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- This imports everything in the lua/plugins/ directory
+    { import = "plugins" },
+  },
+  -- Configure any other settings here
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+})
