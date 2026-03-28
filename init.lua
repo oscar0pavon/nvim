337ac6ff203877b61e82727ac59bb9476ddccbbc
Oscar Pavon <pavon_oscar@outlook.com>
2026-03-28 15:01:10 -0300

    switch to header files

--- init.lua
+++ init.lua
@@ -45,3 +45,16 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+local function clangd_switch_source_header()
+local bufnr = vim.api.nvim_get_current_buf()
+local params = { uri = vim.uri_from_bufnr(bufnr) }
+vim.lsp.buf_request(bufnr, 'textDocument/switchSourceHeader', params, function(err, result)
+if err then vim.notify("LSP Error: " .. tostring(err), vim.log.levels.ERROR) return end
+    if not result then vim.notify("Matching file not found", vim.log.levels.WARN) return end
+        vim.api.nvim_command("edit " .. vim.uri_to_fname(result))
+    end)
+end
+
+ -- Map Ctrl+a to this function
+vim.keymap.set('n', '<C-a>', clangd_switch_source_header, { desc = 'Switch Source/Header' })
+
--- lua/plugins/switch.lua
+++ lua/plugins/switch.lua
@@ -1,17 +1,2 @@
 return {
-  "jakemason/ouroboros.nvim",
-  ft = { "c", "cpp", "h", "hpp" },
-  config = function()
-    require("ouroboros").setup({
-      -- Optional: customize which extensions are preferred
-      extension_preferences_table = {
-        c = { h = 2, hpp = 1 },
-        h = { c = 2, cpp = 1 },
-      },
-    })
-  end,
-  -- Keybindings to trigger the switch
-  keys = {
-    { "<C-a>", "<cmd>Ouroboros<cr>", desc = "Switch Source/Header" },
-  },
 }
