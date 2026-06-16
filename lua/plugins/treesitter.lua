967136f5c9d288d201a355f507198752a3cfeae1
Oscar Pavon <oscar@pavon.com.py>
2026-06-16 16:53:25 +0000

    added treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,5 +20,21 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesitter-context").setup({
+        enable = true, -- Enable this plugin
+        max_lines = 3, -- How many lines the window should span
+        min_window_height = 0, -- Minimum editor window height to enable context
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if max_lines is exceeded
+        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+      })
+    end
+  }
+
 }
 
